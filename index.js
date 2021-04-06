5428921e269f2283bd3b6e3c22afc3f29c4d1ddf
Rhez Danielle <rdpmanliguez@mymail.mapua.edu.ph>
2021-04-07 01:48:56 +0800

    Fixed error banner

--- index.js
+++ index.js
@@ -1,6 +1,20 @@
-window.addEventListener('scroll', () => {
-    const scrollable = document.documentElement.scrollHeight - window.innerHeight;
-    const scrolled = window.scrollY;
+function validate(){
+    var name = document.getElementById("contact-name");
+    var email = document.getElementById("contact-email");
+    var subject = document.getElementById("contact-subject");
+    var message = document.getElementById("contact-message");
+    var error = document.getElementById("contact-error");
 
-    console.log(scrolled);
-});
+    var text;
+    if(name.lenght < 5){
+        text = "Enter a valid Name";
+        error.innerHTML = text;
+        return false;
+    }
+    if(subject.lenght < 5){
+        text = "Enter the message subject"
+        error.innerHTML = text;
+        return false;
+    }
+    
+}
